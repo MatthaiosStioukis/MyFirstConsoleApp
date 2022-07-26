3dc3f1ad7671ddb11a7aec25aeecbc7f969018d0
Matthaios Stioukis <stioukimattigr@gmail.com>
2022-07-26 15:51:02 +0300

    MyFirstConsoleApp which logs hello world using C#

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
