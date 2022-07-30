71f5303c55f46ecb9c47314cb38ef84a8a285f60
Matthaios Stioukis <stioukimattigr@gmail.com>
2022-07-30 14:12:21 +0300

    playing arround with different data types storing them at variables that i have initialized.

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -6,7 +6,29 @@ namespace MyFirstConsoleApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            // This statement declares the variable width with type int and assigns the value 3.
+            int width = 3;
+            // The ++ operator increments a variable (adds 1 to it).
+            width++;
+            // Declared two more varriables to hold numbers
+            // and used the + operator to add and the * operator to multiply.
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+            // The next two statements declare string variables
+            // and use + operator to concatenate them (join them together).
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            // A boolean variable is either true or false
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
         }
     }
 }
